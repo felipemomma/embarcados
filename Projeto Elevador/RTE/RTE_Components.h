b62c01c7c3b3862d80f27231b5f9a3942a36b530
felipemomma <64436335+felipemomma@users.noreply.github.com>
2021-05-13 23:07:11 -0300

    Add Projeto Elevador

---
+++ Projeto Elevador/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: UART_inst
+ * RTE configuration: UART_inst.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "ARMCM4.h"
+
+
+#endif /* RTE_COMPONENTS_H */
